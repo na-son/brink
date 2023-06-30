3bce1eec9ce7cc20eb142a2f174cdde220f9a2ed
Austin Nason <austin.nason@schrodinger.com>
2023-06-29 17:49:04 -0700

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Brink
+
+Very work in progress repo. Not safe for production.
+
+Goals:
+
+Nix-based configuration of physical systems as hypervisors for
+
+something like Nomad, K3s, who knows.
+
---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages.x86_64-linux = {
+      # Cloud Native
+      #amazon = nixos-generators.nixosGenerate {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #    # you can include your own nixos configuration here, i.e.
+      #    # ./configuration.nix
+      #  ];
+      #  format = "amazon";
+      #};
+
+      #gce = nixos-generators.nixosGenerate {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #  ];
+      #  format = "gce";
+      #};
+
+      #docker = nixos-generators.nixosGenerate {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #  ];
+      #  format = "docker";
+      #};
+
+      iso = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        modules = [
+          platforms/iso.nix
+        ];
+        format = "iso";
+      };
+
+      # Virtual Machines
+
+      #vagrant = nixos-generators.nixosGenerate {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #  ];
+      #  format = "vagrant-virtualbox";
+      #};
+
+      #vbox = nixos-generators.nixosGenerate {
+      #  system = "x86_64-linux";
+      #  format = "virtualbox";
+      #};
+
+      #vmware = nixos-generators.nixosGenerate {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #  ];
+      #  format = "vmware";
+      #};
+    };
+  };
+}
+
